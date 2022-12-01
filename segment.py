2c078271e93fb63f4ccfe4c954e1f5a370f9fae4
gregowa <gregowa2401@gmail.com>
2022-12-01 16:29:04 +0100

    qqlq modif

--- segment.py
+++ segment.py
@@ -1,16 +1,19 @@
 #classe définissant un objet de type segment à partir des coordonnées de ces deux extrémités
 from math import sqrt
+import numpy as np
 
 class Segment:
     x1=0
     y1=0
     x2=0
     y2=0
+    seg = [np.linspace(x1, x2), np.linspace(y1, y2)]
     def __init__(self,x1,y1,x2,y2):
         self.x1=x1
         self.y1=y1
         self.x2=x2
         self.y2=y2
+        self.seg=[np.linspace(x1, x2), np.linspace(y1, y2)]
 
     def getX1(self):
         return self.x1
@@ -23,6 +26,12 @@ class Segment:
 
     def getY2(self):
         return self.y2
+
+    def getX1(self):
+        return self.x1
+
+    def getSeg(self):
+        return self.seg
 
     def longueur(self):
         return sqrt((self.x2-self.x1)**2+(self.y2-self.y1)**2)
