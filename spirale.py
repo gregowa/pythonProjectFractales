dca2514a8d3a5f6aa1d289e637fce8d12b5187d6
gregowa <gregowa2401@gmail.com>
2022-12-06 16:58:35 +0100

    spirale

--- spirale.py
+++ spirale.py
@@ -4,40 +4,64 @@ import matplotlib.pyplot as plt
 a = 2
 b = 1.19
 
-phi = np.pi/8
+phi = np.pi / 6
 
 rads = list(reversed(np.arange(0, (8 * np.pi), 0.01)))
 
-x = []
-x1 = []
-x2 = []
-x3 = []
-x4 = []
+pts = []
 
+x = []
 y = []
-y1 = []
-y2 = []
-y3 =[]
-y4= []
 
 for rad in rads:
-    x.append((a * b ** -rad * np.cos(rad)))
-    y.append((a * b ** -rad * np.sin(rad)))
-    x1.append((a * b ** -rad * np.cos(rad)) / 3 + 2)
-    y1.append((a * b ** -rad * np.sin(rad)) / 3)
-    x2.append(((a * b ** -rad * np.cos(rad) / 3 + 2)/3 + 2))
-    y2.append((a * b ** -rad * np.sin(rad) / 3/3))
+    x.append(a * b ** -rad * np.cos(rad))
+    y.append(a * b ** -rad * np.sin(rad))
 
-for i in range(0, len(rads)):
-    x3.append(x1[i] * np.cos(phi) - y1[i] * np.sin(phi))
-    y3.append(x1[i] * np.sin(phi) + y1[i] * np.cos(phi))
-    x4.append(x2[i] * np.cos(phi / 8) - y2[i] * np.sin(phi / 8))
-    y4.append(x2[i] * np.sin(phi / 8) + y2[i] * np.cos(phi / 8))
+pts.append(x)
+pts.append(y)
 
-plt.plot(x, y)
-plt.plot(x1, y1)
-plt.plot(x2, y2)
-plt.plot(x3, y3)
-plt.plot(x4, y4)
-plt.show()
+def h1(x, y):
+    return [x / 3 + 2, y / 3]
 
+
+def h2(x, y):
+    return [(x * np.cos(phi) - y * np.sin(phi)) * (b ** -phi),
+            (x * np.sin(phi) + y * np.cos(phi)) * (b ** -phi)]
+
+def H1(pts) :
+    resX = []
+    resY = []
+    for p in range(0, len(pts[0])):
+        res = h1(pts[0][p], pts[1][p])
+        resX.append(res[0])
+        resY.append(res[1])
+    return [resX, resY]
+
+def H2(pts) :
+    resX = []
+    resY = []
+    for p in range(0, len(pts[0])):
+        res = h2(pts[0][p], pts[1][p])
+        resX.append(res[0])
+        resY.append(res[1])
+    return [resX, resY]
+
+def spirales(pts, n, m):
+    spirale_finale = [pts]
+    for i in range(0, n):
+        spirales_m = []
+        for s in spirale_finale:
+            res1 = H1(s)
+            temp = res1
+            spirales_m.append(temp)
+            for k in range(0, m):
+                res2 = H2(temp)
+                spirales_m.append(res2)
+                temp = res2
+        spirale_finale.clear()
+        spirale_finale = spirales_m
+    for s in spirale_finale:
+        plt.plot(s[0], s[1])
+    plt.show()
+
+spirales(pts, 3, 30)
